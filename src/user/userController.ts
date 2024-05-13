8fbdfcd6a3975339370cbf9f6b41f12bdb0199c1
Satyajeet27 <satyajeet.hitk@gmail.com>
2024-05-13 12:42:15 +0530

    installed bcrypt and added hash function

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -18,6 +19,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       );
       return next(error);
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     res.json({ message: "user created" });
   } catch (error) {}
 };
