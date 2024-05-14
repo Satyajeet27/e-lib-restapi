2405fe7a4dc959073c462c9aa96369f42c8fb132
Satyajeet27 <satyajeet.hitk@gmail.com>
2024-05-14 16:55:48 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,6 +1,51 @@
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  console.log("files", req.files);
+
+  try {
+    //typecasting
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      fileName
+    );
+
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+    //cloudinary mostly image and videos upload ke liye use hota hai, pdf ke liye thoda different h
+
+    const bookFileUpload = await cloudinary.uploader.upload(bookFilePath, {
+      //humlog pdf upload kr rhe h, so resource type raw use kr rhe h, image and video ke case m use nahi hota h
+      resource_type: "raw",
+      filename_override: bookFileName,
+      folder: "book-pdfs",
+      format: "pdf",
+    });
+    console.log("upload bookfile upload", bookFileUpload);
+  } catch (error) {
+    console.log(error);
+    next(createHttpError(500, "Error while uploading the files"));
+  }
+
+  //cloudinary site pr, setting>security idhar check krna hoga pdf and zip files delivery tb humlog console m result dekh paenge else ye upload toh ho jaega but result console m nahi aaega
+
   res.json({});
 };
 
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -8,7 +8,7 @@ const bookRouter = Router();
 const upload = multer({
   //path agar exist nahi krta h toh multer khud se folder create kr dega
   dest: path.resolve(__dirname, "../../public/data/uploads"),
-  limits: { fieldSize: 3e7 }, //3e7 means 30mb: 30*1024*1024
+  limits: { fieldSize: 1e7 }, //3e7 means 30mb: 30*1024*1024
 });
 
 // we are selecting field because we have multiple files, one is coverimage and other ook pdf
