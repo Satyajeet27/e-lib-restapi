1dfc4d9bdbe2a0a6a12f4403f1015b6417637e44
Satyajeet27 <satyajeet.hitk@gmail.com>
2024-05-14 08:37:44 +0530

    setup mukter

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,24 @@
 import { Router } from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "path";
 
 const bookRouter = Router();
 
-bookRouter.post("/", createBook);
+const upload = multer({
+  //path agar exist nahi krta h toh multer khud se folder create kr dega
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fieldSize: 3e7 }, //3e7 means 30mb: 30*1024*1024
+});
+
+// we are selecting field because we have multiple files, one is coverimage and other ook pdf
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default bookRouter;
