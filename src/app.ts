96a7aa40e6d27b25390c61e6ddfbf3d80df3a063
Satyajeet27 <satyajeet.hitk@gmail.com>
2025-05-15 22:11:17 +0530

    added for testing git

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,20 @@ import userRouter from "./user/userRoute";
 import bookRouter from "./book/bookRouter";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   // const error = createHttpError(400, "something went wrong");
   // throw error;
   res.json({ message: "Welcome to api" });
 });
-app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "created for testing git",
+  });
+});
+
 app.use("/api/users", userRouter);
 app.use("/api/books", bookRouter);
 
